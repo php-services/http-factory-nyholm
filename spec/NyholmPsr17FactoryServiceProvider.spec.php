9c5377c0c1172f84fef6e6eeae07f979803d77aa
pmall <pierre.mallinjoud@gmail.com>
2018-11-22 16:06:36 +0100

    add the service provider

---
+++ spec/NyholmPsr17FactoryServiceProvider.spec.php
@@ -0,0 +1,116 @@
+<?php
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+
+use Interop\Container\ServiceProviderInterface;
+
+use Quanta\Container;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Services\Http\Message\NyholmPsr17FactoryServiceProvider;
+
+describe('NyholmPsr17FactoryServiceProvider', function () {
+
+    beforeEach(function () {
+
+        $this->provider = new NyholmPsr17FactoryServiceProvider;
+
+    });
+
+    it('should implement ServiceProviderInterface', function () {
+
+        expect($this->provider)->toBeAnInstanceOf(ServiceProviderInterface::class);
+
+    });
+
+    describe('->getFactories()', function () {
+
+        it('should return an array of length 6', function () {
+
+            $test = $this->provider->getFactories();
+
+            expect($test)->toBeAn('array');
+            expect($test)->toHaveLength(6);
+
+        });
+
+        it('should provide a ServerRequestFactoryInterface entry aliasing the Psr17Factory one', function () {
+
+            $container = new Container($this->provider->getFactories());
+
+            $test = $container->get(ServerRequestFactoryInterface::class);
+
+            expect($test)->toBe($container->get(Psr17Factory::class));
+
+        });
+
+        it('should provide an UriFactoryInterface entry aliasing the Psr17Factory one', function () {
+
+            $container = new Container($this->provider->getFactories());
+
+            $test = $container->get(UriFactoryInterface::class);
+
+            expect($test)->toBe($container->get(Psr17Factory::class));
+
+        });
+
+        it('should provide a StreamFactoryInterface entry aliasing the Psr17Factory one', function () {
+
+            $container = new Container($this->provider->getFactories());
+
+            $test = $container->get(StreamFactoryInterface::class);
+
+            expect($test)->toBe($container->get(Psr17Factory::class));
+
+        });
+
+        it('should provide a ResponseFactoryInterface entry aliasing the Psr17Factory one', function () {
+
+            $container = new Container($this->provider->getFactories());
+
+            $test = $container->get(ResponseFactoryInterface::class);
+
+            expect($test)->toBe($container->get(Psr17Factory::class));
+
+        });
+
+        it('should provide an UploadedFileFactoryInterface entry aliasing the Psr17Factory one', function () {
+
+            $container = new Container($this->provider->getFactories());
+
+            $test = $container->get(UploadedFileFactoryInterface::class);
+
+            expect($test)->toBe($container->get(Psr17Factory::class));
+
+        });
+
+        it('should provide a Psr17Factory entry', function () {
+
+            $container = new Container($this->provider->getFactories());
+
+            $test = $container->get(Psr17Factory::class);
+
+            expect($test)->toEqual(new Psr17Factory);
+
+        });
+
+    });
+
+    describe('->getExtensions()', function () {
+
+        it('should return an empty array', function () {
+
+            $test = $this->provider->getExtensions();
+
+            expect($test)->toBeAn('array');
+            expect($test)->toHaveLength(0);
+
+        });
+
+    });
+
+});
---
+++ src/NyholmPsr17FactoryServiceProvider.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+namespace Services\Http\Message;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+
+use Interop\Container\ServiceProviderInterface;
+
+use Nyholm\Psr7\Factory\Psr17Factory;
+
+final class NyholmPsr17FactoryServiceProvider implements ServiceProviderInterface
+{
+    public function getFactories()
+    {
+        return [
+            ServerRequestFactoryInterface::class => [self::class, 'getServerRequestFactoryInterface'],
+            UriFactoryInterface::class => [self::class, 'getUriFactoryInterface'],
+            StreamFactoryInterface::class => [self::class, 'getStreamFactoryInterface'],
+            UploadedFileFactoryInterface::class => [self::class, 'getUploadedFileFactoryInterface'],
+            ResponseFactoryInterface::class => [self::class, 'getResponseFactoryInterface'],
+            Psr17Factory::class => [self::class, 'getPsr17Factory'],
+        ];
+    }
+
+    public function getExtensions()
+    {
+        return [];
+    }
+
+    public static function getServerRequestFactoryInterface(ContainerInterface $container): ServerRequestFactoryInterface
+    {
+        return $container->get(Psr17Factory::class);
+    }
+
+    public static function getUriFactoryInterface(ContainerInterface $container): UriFactoryInterface
+    {
+        return $container->get(Psr17Factory::class);
+    }
+
+    public static function getStreamFactoryInterface(ContainerInterface $container): StreamFactoryInterface
+    {
+        return $container->get(Psr17Factory::class);
+    }
+
+    public static function getUploadedFileFactoryInterface(ContainerInterface $container): UploadedFileFactoryInterface
+    {
+        return $container->get(Psr17Factory::class);
+    }
+
+    public static function getResponseFactoryInterface(ContainerInterface $container): ResponseFactoryInterface
+    {
+        return $container->get(Psr17Factory::class);
+    }
+
+    public static function getPsr17Factory(): Psr17Factory
+    {
+        return new Psr17Factory;
+    }
+}
